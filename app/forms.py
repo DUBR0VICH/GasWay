051426a5902966c5f4ac7d6dff6b287aab938ca1
DUBR0VICH <andrey.dubrov.04@mail.ru>
2024-05-03 16:13:43 +0300

    initial

---
+++ app/forms.py
@@ -0,0 +1,45 @@
+"""
+Definition of forms.
+"""
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+
+from django.db import models
+from .models import Comment
+from .models import Blog
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Введите пароль'}))
+
+
+class AnketaForm(forms.Form):
+    name = forms.CharField(label='Ваш никнейм', min_length=2, max_length=100)
+    internet = forms.ChoiceField(label='Тема обратной связи:',
+                                 choices=(('1', 'Баги'),
+                                          ('2','Предложения'),
+                                          ('3','Отзыв')), initial=1)
+    notice = forms.BooleanField(label='Получать новости о продуктах на email?',
+                                required=False)
+    email = forms.EmailField(label='Ваш email', min_length=7)
+    message = forms.CharField(label='Поле для текста обратной связи:',
+                              widget=forms.Textarea(attrs={'rows':12,'cols':20}))
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title', 'description', 'content', 'image',)
---
+++ app/views.py
@@ -0,0 +1,206 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from .forms import AnketaForm
+from django.contrib.auth.forms import UserCreationForm
+
+from django.db import models
+from .models import Blog
+
+from .models import Comment
+from .forms import CommentForm
+
+from .forms import BlogForm
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Официальный сайт Gas Way',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Общение',
+            'message':'Ваша страница',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'Описание',
+            'message':'Страница с описанием',
+            'year':datetime.now().year,
+        }
+    )
+
+def links(request):
+    """Renders the links page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/links.html',
+        {
+            'title':'Полезные ресурсы',
+            'year':datetime.now().year,
+        }
+    )
+
+def anketa(request):
+    assert isinstance(request, HttpRequest)
+    data = None
+    internet = {'1': 'Баги', '2': 'Предложения', '3': 'Отзыв'}
+    if request.method == 'POST':
+        form = AnketaForm(request.POST)
+        if form.is_valid():
+            data = dict()
+            data['name'] = form.cleaned_data['name']
+            data['internet'] = internet[ form.cleaned_data['internet'] ]
+            if(form.cleaned_data['notice'] == True):
+                data['notice'] = 'Да'
+            else:
+                data['notice'] = 'Нет'
+            data['email'] = form.cleaned_data['email']
+            data['message'] = form.cleaned_data['message']
+            form = None
+    else:
+        form = AnketaForm()
+    return render(
+        request,
+        'app/anketa.html',
+        {
+            'form':form,
+            'data':data,
+            'title':'Форма обратной связи',
+            'year':datetime.now().year
+        }
+    )
+
+def registration(request):
+    """Renders the registration page."""
+    
+    if request.method == "POST": # после отправки формы
+        regform = UserCreationForm (request.POST)
+        if regform.is_valid(): #валидация полей формы
+            reg_f = regform.save(commit=False) # не сохраняем автоматически данные формы
+            reg_f.is_staff = False # запрещен вход в административный раздел
+            reg_f.is_active = True # активный пользователь
+            reg_f.is_superuser = False # не является суперпользователем
+            reg_f.date_joined = datetime.now() # дата регистрации
+            reg_f.last_login = datetime.now() # дата последней авторизации
+            regform.save() # сохраняем изменения после добавления данных
+            return redirect('home') # переадресация на главную страницу после регистрации
+    else:
+        regform = UserCreationForm() # создание объекта формы для ввода данных нового пользователя
+
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/registration.html',
+        {
+            'regform': regform, # передача формы в шаблон веб-страницы
+            'year':datetime.now().year,
+        }
+    )
+
+def blog(request):
+    """Renders the blog page."""
+    posts = Blog.objects.all() # запрос на выбор всех статей блога из модели
+
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/blog.html',
+        {
+            'title': 'Новости',
+            'posts': posts, #список статей в шаблон страницы
+            'year': datetime.now().year,
+        }
+    )
+
+def blogpost(request, parametr):
+    """Renders the posted blog page."""
+    assert isinstance(request, HttpRequest)
+
+    post_1 = Blog.objects.get(id = parametr) # запрос на выбор конкретной статьи по параметру
+    comments = Comment.objects.filter(post=parametr)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment_f = form.save(commit = False)
+            comment_f.author = request.user
+            comment_f.date = datetime.now()
+            comment_f.post = Blog.objects.get(id=parametr)
+            comment_f.save()
+            return redirect('blogpost', parametr=post_1.id)
+    else:
+        form = CommentForm()
+    return render(
+        request,
+        'app/blogpost.html',
+        {
+            'post_1': post_1, # передача конкретной статьи в шаблон страницы
+            'comments':comments,
+            'form':form,
+            'year': datetime.now().year,
+        }
+    )
+
+def newpost(request):
+    """Renders the newpost page."""
+    assert isinstance(request, HttpRequest)
+
+    if request.method == "POST":
+        blogform = BlogForm(request.POST, request.FILES)
+        if blogform.is_valid():
+            blog_f = blogform.save(commit=False)
+            blog_f.posted = datetime.now()
+            blog_f.author = request.user
+            blog_f.save()
+            return redirect('blog')
+    else:
+        blogform = BlogForm()
+
+    return render(
+        request,
+        'app/newpost.html',
+        {
+            'blogform': blogform,
+            'title': 'Добавить статью',
+            'year': datetime.now().year,
+        }
+    )
+
+def videopost(request):
+    """Renders the videopost page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/videopost.html',
+        {
+            'title':'Описание',
+            'message':'Страница с описанием',
+            'year':datetime.now().year,
+        }
+    )

49504ffc175166c18b0747464fe95165a5c023a3
DUBROVICH <dubrov@endesign.ru>
2025-02-16 20:19:58 +0300

    Final

--- GasWay/settings.py
+++ GasWay/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '850c7d82-a8fa-43fb-a9dd-67222df328e6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
